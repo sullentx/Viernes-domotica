36d8be6c1d8a417785077b4b3f104d0c6c023dde
sullentx <233290@ids.upchiapas.edu.mx>
2025-03-23 22:27:39 -0600

    weather-deatails finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from "./components/sidenav/sidenav.component";
 import { CardComponent } from "./components/card/card.component";
 import { HeaderComponent } from "./components/shared/header/header.component";
+import { WeatherDetailsComponent } from "./components/weather-details/weather-details.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HeaderComponent],
+  imports: [HeaderComponent, WeatherDetailsComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
