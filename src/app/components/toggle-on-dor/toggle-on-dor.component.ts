c2815227f7eeef90a442e0eab6b54ecd5b2166e9
sullentx <233290@ids.upchiapas.edu.mx>
2025-03-29 13:39:16 -0600

    fixing mistakes

--- src/app/components/toggle-on-dor/toggle-on-dor.component.ts
+++ src/app/components/toggle-on-dor/toggle-on-dor.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+
 @Component({
   selector: 'app-toggle-on-dor',
   standalone: true,
-  imports: [MatIcon],
+  imports: [CommonModule, MatIconModule, MatSlideToggleModule],
   templateUrl: './toggle-on-dor.component.html',
-  styleUrl: './toggle-on-dor.component.scss'
+  styleUrls: ['./toggle-on-dor.component.scss']
 })
 export class ToggleOnDorComponent {
- status = false;
+  @Input() status: boolean = false;
+  @Output() statusChange = new EventEmitter<boolean>();
+
+  toggleDoor(event: MatSlideToggleChange): void {
+    this.status = event.checked;
+    this.statusChange.emit(this.status);
+  }
 }
